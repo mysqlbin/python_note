5f5f6e40d1a437e405d1e76fa70efbef90851abf
mysqlbin <2955468423@qq.com>
2019-09-05 18:26:05 +0800

    ./

--- 2019-09-03-datetimes.py
+++ 2019-09-03-datetimes.py
@@ -5,25 +5,25 @@ import time
 import datetime
 
 
-start_time = '2019-06-18 03:00:00'
-start_times = datetime.datetime.strptime(start_time, '%Y-%m-%d %H:%M:%S')
-print(start_times)
-
-
-start_time_a = '2019-06-25 00:30:00'
-start_time_b = datetime.datetime.strptime(start_time_a, '%Y-%m-%d %H:%M:%S')
-print(start_time_b)
+# start_time = '2019-06-18 03:00:00'
+# start_times = datetime.datetime.strptime(start_time, '%Y-%m-%d %H:%M:%S')
+# print(start_times)
+#
+#
+# start_time_a = '2019-06-25 00:30:00'
+# start_time_b = datetime.datetime.strptime(start_time_a, '%Y-%m-%d %H:%M:%S')
+# print(start_time_b)
 
 # 日期减一天
 default_time = time.strftime("%Y-%m-%d")
-default_time = datetime.datetime.strptime(default_time, '%Y-%m-%d') + datetime.timedelta(days=-1)
+default_time = datetime.datetime.strptime(default_time, '%Y-%m-%d') + datetime.timedelta(days=-60)
 print(default_time)
-print(default_time.strftime('%d'))
+# print(default_time.strftime('%d'))
 
 # 日期加一天
-default_current_time = time.strftime("%Y-%m-%d")
-default_current_time = datetime.datetime.strptime(default_current_time, '%Y-%m-%d') + datetime.timedelta(days=1)
-print(default_current_time)
+# default_current_time = time.strftime("%Y-%m-%d")
+# default_current_time = datetime.datetime.strptime(default_current_time, '%Y-%m-%d') + datetime.timedelta(days=1)
+# print(default_current_time)
 
 
 
