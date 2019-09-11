f9e728fdf5ef71f442364f56e320bdde2bb976fb
mysqlbin <38709460+mysqlbin@users.noreply.github.com>
2019-09-11 22:25:46 +0800

    .

--- Python_Core_technology_and_actual_combat/09-custom_function.py
+++ Python_Core_technology_and_actual_combat/09-custom_function.py
@@ -8,9 +8,9 @@ def my_func(message):
     print('Got a message: {}'.format(message))
 
 # 调用函数 my_func()
-# my_func('Hello World')
+my_func('Hello World')
 # 输出
-#Got a message: Hello World
+# Got a message: Hello World
 
 def name(param1, param2, ..., paramN):
     statements
@@ -50,7 +50,6 @@ def find_largest_element(l):
             largest_element = item
     print('largest element is: {}'.format(largest_element))
 
-
 find_largest_element([8, 1, -3, 2, 0])
 """
     定义了函数 find_largest_element，作用是遍历输入的列表，找出最大的值并打印。
@@ -63,14 +62,14 @@ largest element is: 8
 
 
 #函数要在调用之前先声明好
-"""
+
 my_func('hello world')
 def my_func(message):
     print('Got a message: {}'.format(message))
-"""
 
-# # 输出
-# NameError: name 'my_func' is not defined
+
+# 输出
+NameError: name 'my_func' is not defined
 
 
 #　函数内部调用其它函数, 函数间哪个声明在前, 哪个在后就无所谓, 因为 def 是可执行语句, 函数在调用之前都不存在, 只需要保证调用时, 所需的函数都已经声明定义
@@ -83,28 +82,28 @@ def my_sub_func(message):
 my_func('hello world')
 
 # 输出
-#Got a message: hello world
+Got a message: hello world
 
 
 # 函数可以设定默认值，在调用函数 func() 时，如果参数 param 没有传入，则参数默认为 0；而如果传入了参数 param，其就会覆盖默认值。
-"""
+
 def func(param = 0):
     ...
-"""
+
 
 # Python 是 dynamically typed 的, 可以接受任何数据类型(整型, 浮点, 字符串, 列表等等)的参数
 
 #　把列表作为参数传递，表示将两个列表相连接
 print(my_sum([1, 2], [3, 4]))
 # 输出
-# [1, 2, 3, 4]
+[1, 2, 3, 4]
 
 
 # 字符串作为参数传递，表示让两个字符串相合并拼接
 print(my_sum('hello ', 'world'))
 
 # 输出
-#  hello world
+hello world
 
 
 #函数支持嵌套：
@@ -118,12 +117,43 @@ def f1():
 f1()
 
 # 输出
-"""
 hello
 world
-"""
+
 # 函数 f1() 的内部，又定义了函数 f2()。在调用函数 f1() 时，会先打印字符串 ‘hello’, ，然后 f1() 内部再调用 f2()，打印字符串 'world'
 # 函数嵌套的作用：
 
+def connect_DB():
+    def get_DB_configuration():
+        ...
+        return host, username, password
+    conn = connector.connect(get_DB_configuration())
+    return conn
+
+get_DB_configuration()
+# 输出
+NameError: name 'get_DB_configuration' is not defined
 
 
+def read_text_from_file(file_path):
+    with open(file_path) as file:
+        ...
+
+
+MIN_VALUE = 1
+MAX_VALUE = 10
+def validation_check(value):
+    if value < MIN_VALUE or value > MAX_VALUE:
+        raise Exception('validation check fails')
+
+
+MIN_VALUE = 1
+MAX_VALUE = 10
+def validation_check(value):
+    ...
+    MIN_VALUE += 1
+    ...
+validation_check(5)
+
+# 输出报错信息
+UnboundLocalError: local variable 'MIN_VALUE' referenced before assignment
