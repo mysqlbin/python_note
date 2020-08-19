732bc95efe24ddadd114386f6808acfa9f5f39f0
mysqlbin <38709460+mysqlbin@users.noreply.github.com>
2020-08-19 23:34:52 +0800

    .

--- 2020-08-01-Python-ZST-4200/03-配置文件与日志/2020-08-12-logger_trace.py
+++ 2020-08-01-Python-ZST-4200/03-配置文件与日志/2020-08-12-logger_trace.py
@@ -1,26 +1,26 @@
 import logging
 
 logger = logging.getLogger('logger_test')    # 每行日志记录开头的名称
-logger.setLevel('DEBUG')        # 设置日志记录级别，把日志记录级别大于等于DEBUG的记录下来
+logger.setLevel(logging.DEBUG)        # 设置日志记录级别，把日志记录级别大于等于DEBUG的记录下来
 
 BASIC_FORMAT = "%(name)s %(asctime)s %(filename)s:%(lineno)s %(levelname)s %(message)s"    # 设置日志记录的格式
 DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
 
-# formatter = logging.Formatter(BASIC_FORMAT, DATE_FORMAT)  # 设置日志记录的格式
-formatter = logging.Formatter(BASIC_FORMAT)  # 设置日志记录的格式
+formatter = logging.Formatter(BASIC_FORMAT, DATE_FORMAT)  # 设置日志记录的格式
+# formatter = logging.Formatter(BASIC_FORMAT)  # 设置日志记录的格式
 
-chlr = logging.StreamHandler()  # 把日志输出到控制台的Handler
-chlr.setFormatter(formatter)
-chlr.setLevel('DEBUG')          # 也可以不设置，不设置就默认用logger的level
+# chlr = logging.StreamHandler()  # 把日志输出到控制台的Handler
+# chlr.setFormatter(formatter)
+# chlr.setLevel('DEBUG')          # 也可以不设置，不设置就默认用logger的level
 
 fhlr = logging.FileHandler('example.log')  # 把日志写入到文件的handler
 fhlr.setFormatter(formatter)
-fhlr.setLevel('INFO')           # 设置日志记录级别，把日志记录级别大于等于INFO的记录下来
+# fhlr.setLevel('INFO')           # 设置日志记录级别，把日志记录级别大于等于INFO的记录下来
 
-logger.addHandler(chlr)
+# logger.addHandler(chlr)
 logger.addHandler(fhlr)
 
 if __name__ == "__main__":
 
-   logger.info('this is a info message')    # 记录日志记录的内容
+   # logger.info('this is a info message')    # 记录日志记录的内容
    logger.debug('this is a debug message')
