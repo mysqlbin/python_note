c33a02767c7f7c4b3c433db2af4c37647a498f59
mysqlbin <38709460+mysqlbin@users.noreply.github.com>
2020-11-08 23:44:18 +0800

    .

---
+++ 2020-09-05-Python-ZST-4200-new/2020-11-07/mysql_front_1107/src/views/Login.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+      <form action="">
+            
+            <div class="form-item form-item2" >
+                <input type="text" name="username" v-model="login.username">
+            </div>
+            <div class="form-item">
+                <input type="password" name="password" >
+            </div>
+            <div class="form-item">
+                <button type="button" @click="handleLogin">登录</button>
+            </div>
+        </form>
+        <!-- <div id="main" style="height: 400px;"></div> -->
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Login',
+    data(){
+      return {
+        login: {
+          username: "",
+          password: "",
+        }
+      }
+    },
+    methods: {
+      handleLogin(){
+        console.log(this.login.username)
+      }  
+    }
+  }
+</script>
+
+<style scoped>
+  .form-item {
+    margin-bottom: 20px;
+  }
+</style>
