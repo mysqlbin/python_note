9796e83546c4c2f4dba2cd4ae629d8b584142e6a
mysqlbin <1224056230@qq.com>
2020-12-10 10:52:04 +0800

    .

--- 2020-09-05-Python-ZST-4200-new/2020-12-04/2020-12-04-虚拟环境的搭建.sql
+++ 2020-09-05-Python-ZST-4200-new/2020-12-04/2020-12-04-虚拟环境的搭建.sql
@@ -84,11 +84,79 @@ Linux 安装pipenv
 
 
 
+==> default: Available bridged network interfaces:
+1) Realtek PCIe GBE Family Controller
+2) Npcap Loopback Adapter
+==> default: When choosing an interface, it is usually the one that is
+==> default: being used to connect to the internet.
+==> default:
+    default: Which interface should the network bridge to? y
+    default: Which interface should the network bridge to? y
+    default: Which interface should the network bridge to? YES
+    default: Which interface should the network bridge to? 1
+==> default: Preparing network interfaces based on configuration...
+    default: Adapter 1: nat
+    default: Adapter 2: hostonly
+    default: Adapter 3: bridged
+==> default: Forwarding ports...
+    default: 22 (guest) => 2222 (host) (adapter 1)
+==> default: Booting VM...
+==> default: Waiting for machine to boot. This may take a few minutes...
+    default: SSH address: 127.0.0.1:2222
+    default: SSH username: vagrant
+    default: SSH auth method: private key
 
+-----------------------------------------
 
-
-
-
-
-
-
+E:\centos\centos-new4                                                                          
+λ vagrant reload                                                                               
+==> default: You assigned a static IP ending in ".1" to this machine.                          
+==> default: This is very often used by the router and can cause the                           
+==> default: network to not work properly. If the network doesn't work                         
+==> default: properly, try changing this IP.                                                   
+==> default: Attempting graceful shutdown of VM...                                             
+==> default: You assigned a static IP ending in ".1" to this machine.                          
+==> default: This is very often used by the router and can cause the                           
+==> default: network to not work properly. If the network doesn't work                         
+==> default: properly, try changing this IP.                                                   
+==> default: Clearing any previously set forwarded ports...                                    
+==> default: Clearing any previously set network interfaces...                                 
+==> default: Available bridged network interfaces:                                             
+1) Realtek PCIe GBE Family Controller                                                          
+2) Npcap Loopback Adapter                                                                      
+==> default: When choosing an interface, it is usually the one that is                         
+==> default: being used to connect to the internet.                                            
+==> default:                                                                                   
+    default: Which interface should the network bridge to? 3                                   
+    default: Which interface should the network bridge to? 1                                   
+==> default: Preparing network interfaces based on configuration...                            
+    default: Adapter 1: nat                                                                    
+    default: Adapter 2: hostonly                                                               
+    default: Adapter 3: bridged                                                                
+==> default: Forwarding ports...                                                               
+    default: 22 (guest) => 2222 (host) (adapter 1)                                             
+==> default: Booting VM...                                                                     
+==> default: Waiting for machine to boot. This may take a few minutes...                       
+    default: SSH address: 127.0.0.1:2222                                                       
+    default: SSH username: vagrant                                                             
+    default: SSH auth method: private key                                                      
+    default: Warning: Connection reset. Retrying...                                            
+    default: Warning: Connection aborted. Retrying...                                          
+    default: Warning: Remote connection disconnect. Retrying...                                
+==> default: Machine booted and ready!                                                         
+==> default: Checking for guest additions in VM...                                             
+    default: The guest additions on this VM do not match the installed version of              
+    default: VirtualBox! In most cases this is fine, but in rare cases it can                  
+    default: prevent things such as shared folders from working properly. If you see           
+    default: shared folder errors, please make sure the guest additions within the             
+    default: virtual machine match the version of VirtualBox you have installed on             
+    default: your host and reload your VM.                                                     
+    default:                                                                                   
+    default: Guest Additions Version: 4.3.30                                                   
+    default: VirtualBox Version: 5.2                                                           
+==> default: Configuring and enabling network interfaces...                                    
+==> default: Mounting shared folders...                                                        
+    default: /home/vagrant/src => E:/centos/centos-new4/data/src                               
+    default: /home/vagrant/software => E:/centos/centos-new4/data/software                     
+==> default: Machine already provisioned. Run `vagrant provision` or use the `--provision`     
+==> default: flag to force provisioning. Provisioners marked to run always will still run.     
