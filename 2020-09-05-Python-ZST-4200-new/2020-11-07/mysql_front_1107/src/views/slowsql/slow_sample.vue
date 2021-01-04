e4592ad6f75bd98bcfefd834a1279427ccb6d148
mysqlbin <38709460+mysqlbin@users.noreply.github.com>
2021-01-04 23:32:56 +0800

    ;

--- 2020-09-05-Python-ZST-4200-new/2020-11-07/mysql_front_1107/src/views/slowsql/slow_sample.vue
+++ 2020-09-05-Python-ZST-4200-new/2020-11-07/mysql_front_1107/src/views/slowsql/slow_sample.vue
@@ -19,47 +19,75 @@ export default {
     },
     created(){
         console.log("created: ", 'created')
+
+        this.updateQueryParams = _.debounce(
+            function (startTime, endTime) {
+                this.queryParams.start = startTime;
+                this.queryParams.end = endTime;
+                console.log(this.queryParams);
+                this.$emit("updateQueryParams");
+            }.bind(this),
+            1000
+        );
+
+        this.$on(
+            "updateQueryParams",
+            function () {
+                console.log("query params has changed");
+                // this.doSearch();
+            }.bind(this)
+        );
+
     },
     mounted(){
         console.log("mounted: ", 'mounted')
         this.createChart();
     },
     methods: {
+
         createChart() {
-            
+            let that = this;
             Highcharts.setOptions({
                 lang: {
                     rangeSelectorZoom: "",
                 },
             });
 
-            // $.getJSON('https://data.jianshukeji.com/jsonp?filename=json/aapl-c.json&callback=?', function (data) {
-            $.getJSON('https://demo-live-data.highcharts.com/aapl-c.json', function (data) {
-                // Create the chart
-                Highcharts.stockChart('container',  {
+            getAggsByDate(this.queryParams).then((resp) => {
+
+                let chartData = [];
+                // 组装数据，渲染到图表中
+                _.forEach(resp.data, (v) => {
+                    chartData.push([moment(v["date"]).unix() * 1000, v["date_count"]]);
+                });
+                console.log("chartData", chartData);
+
+                this.stockChart = new Highcharts.stockChart("container", {
+
                     rangeSelector: {
                         selected: 1
                     },
                     title: {
-                        text: 'AAPL Stock Price'
+                        text: '每日慢SQL数量'
                     },
                     xAxis: {
                         events: {
-                        // 监听时间范围的变化
-                        afterSetExtremes: function (e) {
-                            // e.min 和 e.max 为坐标轴当前的范围
-                            console.log(e.min, e.max);
-                            let startTime = _.toInteger(e.min) / 1000;
-                            let endTime = _.toInteger(e.max) / 1000;
-                            startTime = moment.unix(startTime).format();
-                            endTime = moment.unix(endTime).format();
-                            console.log("startTime: ", startTime);
-                            console.log("endTime: ", endTime);
-                        },
+                            // 监听时间范围的变化
+                            afterSetExtremes: function (e) {
+                                // e.min 和 e.max 为坐标轴当前的范围
+                                console.log(e.min, e.max);
+                                let startTime = _.toInteger(e.min) / 1000;
+                                let endTime = _.toInteger(e.max) / 1000;
+                                startTime = moment.unix(startTime).format();
+                                endTime = moment.unix(endTime).format();
+                                console.log("startTime: ", startTime);
+                                console.log("endTime: ", endTime);
+                                that.updateQueryParams(startTime, endTime);
+                            },
                         },
                     },
                     series: [{
-                        name: 'AAPL Stock Price',
+                        name: '数量',
                         data: data,
                         type: 'spline',
                         tooltip: {
@@ -69,9 +97,68 @@ export default {
                     
                 });
 
-                console.log("data: ", data)
-            });
+            })
+
+
+            // $.getJSON('https://data.jianshukeji.com/jsonp?filename=json/aapl-c.json&callback=?', function (data) {
+                
+
+
+
         },
+        doSearch(){
+            getTop10(this.queryParams).then(resp=> {
+                this.tableData = resp.data
+            })
+        },
+
+        // createChart() {
+            
+        //     Highcharts.setOptions({
+        //         lang: {
+        //             rangeSelectorZoom: "",
+        //         },
+        //     });
+
+        //     // $.getJSON('https://data.jianshukeji.com/jsonp?filename=json/aapl-c.json&callback=?', function (data) {
+        //     $.getJSON('https://demo-live-data.highcharts.com/aapl-c.json', function (data) {
+        //         // Create the chart
+        //         Highcharts.stockChart('container',  {
+        //             rangeSelector: {
+        //                 selected: 1
+        //             },
+        //             title: {
+        //                 text: 'AAPL Stock Price'
+        //             },
+        //             xAxis: {
+        //                 events: {
+        //                 // 监听时间范围的变化
+        //                 afterSetExtremes: function (e) {
+        //                     // e.min 和 e.max 为坐标轴当前的范围
+        //                     console.log(e.min, e.max);
+        //                     let startTime = _.toInteger(e.min) / 1000;
+        //                     let endTime = _.toInteger(e.max) / 1000;
+        //                     startTime = moment.unix(startTime).format();
+        //                     endTime = moment.unix(endTime).format();
+        //                     console.log("startTime: ", startTime);
+        //                     console.log("endTime: ", endTime);
+        //                 },
+        //                 },
+        //             },
+        //             series: [{
+        //                 name: 'AAPL Stock Price',
+        //                 data: data,
+        //                 type: 'spline',
+        //                 tooltip: {
+        //                     valueDecimals: 2
+        //                 }
+        //             }],
+                    
+        //         });
+
+        //         console.log("data: ", data)
+        //     });
+        // },
     
     }
 }
