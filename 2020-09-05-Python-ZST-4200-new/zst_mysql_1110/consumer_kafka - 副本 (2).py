f2b25eaff594c37c8d0ecc59185fc1b53d74e22a
mysqlbin <1224056230@qq.com>
2021-01-12 16:54:01 +0800

    ,

--- 2020-09-05-Python-ZST-4200-new/zst_mysql_1110/consumer_kafka - 副本 (2).py
+++ 2020-09-05-Python-ZST-4200-new/zst_mysql_1110/consumer_kafka - 副本 (2).py
@@ -33,51 +33,55 @@ if __name__ == '__main__':
         # print("got a message", message.offset, )
         # print("got a message", message.value )
 
-        try:
+        val = str(message.value, encoding='utf-8')
 
-            val = str(message.value, encoding='utf-8')
-            # 将消息反序列化为python对象
-            obj = json.loads(val)
-            print("got a obj", obj.keys())
+        # 将消息反序列化为python对象
+        obj = json.loads(val)
+        print("got a obj：", obj)
 
-            # obj.keys(): 返回一个字典所有的键。
-            if "slowlog.query" not in obj.keys():
-                logging.warning("invalid kafka message: %s", obj)
-                continue
+        if "slowlog.query" not in obj.keys():
+            # logging.warning("invalid kafka message: %s", obj)
+            continue
 
+        # 获取指纹的api
+        url = "http://192.168.0.45:8090/api"
+
+        payload = {
+            "sql": obj['slowlog.query']
+        }
+        payload = json.dumps(payload)
+        headers = {
+            'Content-Type': 'application/json'
+        }
+
+        response = requests.request("POST", url, headers=headers, data=payload)
+
+
+        if response.status_code == 200:
+
+            print(1)
+            # print(response.text)
+            # 获取指纹
+            finger_print_info = json.loads(response.text)
+            finger_print = finger_print_info["finger_print"]
+            # print(finger_print)
+
+            # 获取schema信息
             try:
-
-                # 获取指纹的api
-                url = "http://192.168.0.45:8090/api"
-
-                payload = {
-                    "sql": obj['slowlog.query']
-                }
-                payload = json.dumps(payload)
-                headers = {
-                    'Content-Type': 'application/json'
-                }
-
-                response = requests.request("POST", url, headers=headers, data=payload)
-
                 # 通过api进行查询，获取schema信息
                 url_schema = 'http://192.168.0.45:8001/api/meta_manage/v1/get_schema_by_ip?ip={}'.format(
                     obj['host']['ip'][0])
                 response_url_schema = requests.get(url_schema, headers={'Content-Type': 'application/json'})
-
-                if response.status_code != 200 or response_url_schema.status_code != 200:
+                # print(response_url_schema)
+                if response_url_schema.status_code != 200:
                     logging.error("got none-200 response: %d", response.status_code)
                     continue
 
                 respJson = json.loads(response_url_schema.text)
-
+                print(respJson)
                 if respJson['code'] != 2000:
-                    logging.error("got none-2000 code: %d, message: %s", respJson['code'], respJson['message'])
+                    logging.error("got none-200 code: %d, message: %s", respJson['code'], respJson['message'])
                     continue
-
-                # 获取指纹
-                finger_print_info = json.loads(response.text)
-                finger_print = finger_print_info["finger_print"]
 
                 es_data = {}
                 for k, v in obj.items():
@@ -100,10 +104,3 @@ if __name__ == '__main__':
             except requests.exceptions.ConnectionError as ce:
                 logging.error("failed to query schema: %s", ce)
                 pass
-
-        except json.decoder.JSONDecodeError as jde:
-            logging.error("failed to parse kafka message: %s", jde)
-            pass
-
-
-
