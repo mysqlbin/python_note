aef34889789e5390cc1515f519a43cd4f0588ba5
mysqlbin <13202095158@163.com>
2022-02-22 23:14:45 +0800

    .

---
+++ 2020-09-05-Python-ZST-4200-new/2020-09-19-Python基础/2020-09-19 - data_type_nodata_judge.py
@@ -0,0 +1,27 @@
+
+"""
+
+"""
+
+if __name__ == '__main__':
+
+    print(".....................")
+    lists = []
+    if lists:
+        print('list has data')
+    else:
+        print("list no data")
+
+    tuples = ()
+    if tuples:
+        print('tuple has data')
+    else:
+        print("tuple no data")
+
+    dicts = {}
+    if dicts:
+        print('dict has data')
+    else:
+        print("dict no data")
+
+
